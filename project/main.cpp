55e815594c8fef9381aaf797cc8b05db57ecf46b
topaz11100 <topaz11100@gmail.com>
2024-11-13 22:17:11 +0900

    중간보고서쓰기

--- project/main.cpp
+++ project/main.cpp
@@ -4,6 +4,29 @@
 #include "file_manage.h"
 
 int main() {
+
+	refrigerator r;
+	
+	for (int i = 0; i < 5; i += 1)
+	{
+		vector<string> v;
+		cout << "가져올 음식 이름을 입력하세요" << endl;
+		input_to_vector(cin, v);
+		r.push(get_food(v[0]));
+		r.print();
+	}
+
+	cout << "1 second later" << endl;
+	r.minus_expiry();
+	r.print();
+
+	cout << "5 second later" << endl;
+	for (int i = 0; i < 5; i += 1)
+	{
+		r.minus_expiry();
+	}
+	r.print();
+
 	return 0;
 }
 
